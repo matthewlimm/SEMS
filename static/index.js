02b8e2b2ab4a7f7b9544a1a6856f2a80dc039fd5
XayHanmonty <hanmontybank@gmail.com>
2023-10-31 00:49:58 -0700

    setup workflow

--- static/index.js
+++ static/index.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAT3TKqTwfpeUfICN7PaVwMmPpWRmTTvUw",
+  authDomain: "engineering-be843.firebaseapp.com",
+  projectId: "engineering-be843",
+  storageBucket: "engineering-be843.appspot.com",
+  messagingSenderId: "813909430707",
+  appId: "1:813909430707:web:187ccdccf7a96c6ae3bbc6",
+  measurementId: "G-X0JB8FPHQQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 var temperatureHistoryDiv = document.getElementById("temperature-history");
 var humidityHistoryDiv = document.getElementById("humidity-history");
 
