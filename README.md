1fa88f66cdbe151085ebbfb7c2b6afd506b33734
MjlDoesGithub <matthewlim@berkeley.edu>
2024-06-30 23:27:33 -0700

    organization and readme

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-Hello World!
+# Smart Environmental Monitoring Sensor (SEMS)
+A multi-modular environmental sensor project using Python, Flask, Flask-SocketIO, and Bootstrap that displays real-time temperature, air quality, radiation, and other sensor readings using your Raspberry Pi.
 
-Alpha Theta Smart Environmental Monitoring Sensors
+![SEMS](img/SEMS.gif) 
+  
+## Writeup
+https://abhinavballa.github.io/SEMSweb/
+
+## Prerequisites
+Make sure to install the necessary drivers on your Raspberry Pi.
+  
+## Steps on how to run on Raspberry Pi
+1. Clone the repository
+```
+git clone https://github.com/matthewlimm/SEMS
+```
+2. Create a Python virtual environment
+```
+python -m venv .venv
+source .venv/bin/activate
+```
+3. Install the dependencies
+```
+pip install -r requirements.txt
+```
+
+4. Run the application
+```
+flask run --host=0.0.0.0
+```
+5. Access the application using the following URL
+```
+http://<IP>:5000
+```
--- mq135_module.py
+++ mq135_module.py
@@ -1,22 +1,22 @@
-from guizero import App, Box, Text, TextBox, PushButton, ButtonGroup, MenuBar, info, yesno, warn
-from time import sleep
-from subprocess import call 
-import RPi.GPIO as GPIO
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+# from guizero import App, Box, Text, TextBox, PushButton, ButtonGroup, MenuBar, info, yesno, warn
+# from time import sleep
+# from subprocess import call 
+# import RPi.GPIO as GPIO
+# import busio
+# import digitalio
+# import board
+# import adafruit_mcp3xxx.mcp3008 as MCP
+# from adafruit_mcp3xxx.analog_in import AnalogIn
 import random
 
-# Create the SPI bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-# Create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D22)
-# Create the mcp object
-mcp = MCP.MCP3008(spi, cs)
-# Create analog inputs connected to the input pins on the MCP3008.
-channel_0 = AnalogIn(mcp, MCP.P0)
+# # Create the SPI bus
+# spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+# # Create the cs (chip select)
+# cs = digitalio.DigitalInOut(board.D22)
+# # Create the mcp object
+# mcp = MCP.MCP3008(spi, cs)
+# # Create analog inputs connected to the input pins on the MCP3008.
+# channel_0 = AnalogIn(mcp, MCP.P0)
 
 class MQ135Module:
     def __init__(self, id):
@@ -36,21 +36,21 @@ class MQ135Module:
             return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
             
         while True:
-            # Test your module, then define the value range - in this case between 0 and 60000.
-            sensorValue = _range(channel_0.value, 0, 60000, 0, 1023)
-            # print("AQI:", sensorValue)
-            APL = ""
-            if (sensorValue > 0 & sensorValue <= 50):
-                APL = 0
-            elif (sensorValue >= 51 & sensorValue <= 100):
-                APL = 1
-            elif (sensorValue >= 101 & sensorValue <= 150):
-                APL = 2
-            elif (sensorValue >= 151 & sensorValue <= 200):
-                APL = 3
-            elif (sensorValue >= 201 & sensorValue <= 300):
-                APL = 4
-            else: 
-                APL = 5
-            return sensorValue, APL
-            # return random.randint(0,5), random.randint(0, 5)
+            # # Test your module, then define the value range - in this case between 0 and 60000.
+            # sensorValue = _range(channel_0.value, 0, 60000, 0, 1023)
+            # # print("AQI:", sensorValue)
+            # APL = ""
+            # if (sensorValue > 0 & sensorValue <= 50):
+            #     APL = 0
+            # elif (sensorValue >= 51 & sensorValue <= 100):
+            #     APL = 1
+            # elif (sensorValue >= 101 & sensorValue <= 150):
+            #     APL = 2
+            # elif (sensorValue >= 151 & sensorValue <= 200):
+            #     APL = 3
+            # elif (sensorValue >= 201 & sensorValue <= 300):
+            #     APL = 4
+            # else: 
+            #     APL = 5
+            # return sensorValue, APL
+            return random.randint(0,5), random.randint(0, 5)
